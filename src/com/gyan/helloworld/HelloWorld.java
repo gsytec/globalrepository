389ec8fe82f9a837070938c99eb053ae6fdd1b82
gsytec <gsytec@gmail.com>
2016-01-15 12:14:48 +0530

    commit01 by Gyanendra

---
+++ src/com/gyan/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.gyan.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Ant");
+
+	}
+
+}
